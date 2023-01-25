05fd62d45fcf7a63670f9fa6eeb2e13bd3698ebe
Michael Lamparski <diagonaldevice@gmail.com>
2023-01-25 17:34:41 -0500

    enable usage of current-levels file

--- compute-totals.py
+++ compute-totals.py
@@ -15,24 +15,52 @@ def main():
         description='compute total weapon upgrade requirements',
     )
     parser.add_argument('--file', default='weapon-upgrades.yaml', help='ingredients yaml file')
+    parser.add_argument('--current-levels', metavar='YAMLFILE', help='read current levels from a YAML file, so that the displayed counts do not count upgrades you already have')
     parser.add_argument('--markdown', action='store_true')
     args = parser.parse_args()
 
-    with open(args.file) as f:
-        weapons = yaml.load(f)
+    weapons = read_weapons(args.file)
+    current_levels = None
+    if args.current_levels is not None:
+        current_levels = read_current_levels(args.current_levels)
+        validate_current_levels(weapons, current_levels, current_levels_path=args.current_levels)
 
-    counts = count_ingredients(weapons)
+    counts = count_ingredients(weapons, current_levels)
     if args.markdown:
         display_counts_md(counts)
     else:
         display_counts(counts)
 
-def count_ingredients(weapons: list):
+def read_weapons(path: str):
+    with open(path) as f:
+        return yaml.load(f)
+
+def read_current_levels(path: str) -> dict[str, int]:
+    with open(path) as f:
+        d = yaml.load(f)
+    if not isinstance(d, dict):
+        die(f'{path}: file must be a YAML mapping from weapon names to level numbers, got a {type(d)}')
+    for key, level in d.items():
+        if not isinstance(level, int):
+            die(f'{path}: at {repr(key)}: level must be integer')
+        if level < 1:
+            die(f'{path}: at {repr(key)}: minimum level is 1')
+    return d
+
+def count_ingredients(weapons: list, current_levels: dict[str, int] | None):
     counter = Counter()
     for weapon in weapons:
-        for upgrade in weapon['ingredients']:
+        name = weapon['name']
+        skipped_levels = 0 if current_levels is None else current_levels.get(name, 1) - 1
+        for upgrade in weapon['ingredients'][skipped_levels:]:
             counter.update(upgrade)
     return counter
+
+def validate_current_levels(weapons: list, current_levels: dict[str, int], current_levels_path: str):
+    all_names = set(weapon['name'] for weapon in weapons)
+    for name in current_levels:
+        if name not in all_names:
+            die(f'{current_levels_path}: {repr(name)} is not a known weapon')
 
 def display_counts(counts: Counter[str]):
     maxlen = max(len(key) for key in counts)
